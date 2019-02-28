bda27f29f7d81c49f7b6983254e81f8390fb0034
Chase Douglas <chase@stackery.io>
2019-02-28 10:03:19 -0800

    Implement getUnicorn

--- src/getUnicorn/index.js
+++ src/getUnicorn/index.js
@@ -1,5 +1,56 @@
+const productionFleet = [
+  {
+    Name: 'Bucephalus',
+    Color: 'Golden',
+    Gender: 'Male'
+  },
+  {
+    Name: 'Shadowfax',
+    Color: 'White',
+    Gender: 'Male'
+  },
+  {
+    Name: 'Rocinante',
+    Color: 'Yellow',
+    Gender: 'Female'
+  }
+];
+
+const stagingFleet = [
+  {
+    Name: 'Norman',
+    Color: 'Beige',
+    Gender: 'Male'
+  },
+  {
+    Name: 'Stewart',
+    Color: 'Taupe',
+    Gender: 'Male'
+  },
+  {
+    Name: 'Florence',
+    Color: 'Parchment',
+    Gender: 'Female'
+  }
+]
+
 exports.handler = async message => {
   console.log(message);
 
-  return {};
+  let unicorn;
+  if (message.requestContext.stageVariables && message.requestContext.stageVariables.stage === 'production') {
+    unicorn = productionFleet[Math.floor(Math.random() * fleet.length)];
+  } else {
+    unicorn = stagingFleet[Math.floor(Math.random() * fleet.length)];
+  }
+
+  console.log(`Sending unicorn: ${unicorn}`);
+
+  return {
+    statusCode: 200,
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(unicorn)
+  };
 }
