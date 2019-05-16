95a8a905192af76731b2d68bcee30e9255913092
gal bashan <gal@epsagon.com>
2019-05-16 14:33:51 +0300

    [unicorn-details] Fixing functions

--- src/getUnicornDetails/index.js
+++ src/getUnicornDetails/index.js
@@ -1,47 +1,71 @@
-const productionFleet = [
-  {
+const productionFleet = {
+  Bucephalus: {
     Name: 'Bucephalus',
     Color: 'Golden',
     Gender: 'Male'
   },
-  {
+  Shadowfax: {
     Name: 'Shadowfax',
     Color: 'White',
     Gender: 'Male'
   },
-  {
+  Rocinante: {
     Name: 'Rocinante',
     Color: 'Yellow',
     Gender: 'Female'
   }
-];
+};
 
-const stagingFleet = [
-  {
+const stagingFleet = {
+  Norman: {
     Name: 'Norman',
     Color: 'Beige',
     Gender: 'Male'
   },
-  {
+  Stewart: {
     Name: 'Stewart',
     Color: 'Taupe',
     Gender: 'Male'
   },
-  {
+  Florence: {
     Name: 'Florence',
     Color: 'Parchment',
     Gender: 'Female'
   }
-]
+}
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
 
 exports.handler = async message => {
   console.log(message);
 
   const fleet = (message.requestContext.stageVariables && message.requestContext.stageVariables.stage === 'production') ?
     productionFleet :
-    stagingFleet;
+        stagingFleet;
+  const delayedName = (message.requestContext.stageVariables && message.requestContext.stageVariables.stage === 'production') ?
+    'Rocinante' : 'Norman';
 
-  const unicorn = fleet[Math.floor(Math.random() * fleet.length)];
+  const unicornName = message.pathParameters.Name;
+  const unicorn = fleet[unicornName];
+  if (!unicorn) {
+    return {
+      statusCode: 404,
+       body: 'Not Found'
+    }
+  }
+
+  unicorn.Email = `${unicornName.toLowerCase()}@wildrydes.com`;
+
+  console.log(`name: ${unicornName}, delayed: ${delayedName}`);
+  if (unicornName === delayedName) {
+    if (Math.random() >= 0.5) {
+        await sleep(6000);
+    } else {
+        await sleep(4000);
+    }
+  }
 
   console.log(`Sending unicorn: ${JSON.stringify(unicorn, null, 2)}`);
 
@@ -52,12 +76,4 @@ exports.handler = async message => {
     },
     body: JSON.stringify(unicorn)
   };
-}
-
-export detailsHandler = async event => {
-    console.log(event);
-    return {
-        statusCode: 200,
-        body: json.stringify(event)
-    }
 }
